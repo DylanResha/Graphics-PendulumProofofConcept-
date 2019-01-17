0c2d984b78fea4e8de7cd61422e4518e1af97624
Dylan Resha <resha_dt@gsc_218_22.mercer.edu>
2019-01-17 12:02:03 -0500

    started fixing main

--- main.cc
+++ main.cc
@@ -4,6 +4,17 @@
 //main for objects
 int main(int argc, char** argv)
 {
+
+   double t=0.0;
+   double theta=M_PI/4.0;
+   double omega=0.0;
+
+   for (long i=0; i<2*Nstep; i++ ) {
+      printf("%f %f %f\n", t, theta, omega);
+      step( &t, &theta, &omega);
+   }
+
+
     glutInit(&argc, argv);
     glutInitDisplayMode ( GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH );
     glutInitWindowSize (500, 500); 
@@ -16,6 +27,7 @@ int main(int argc, char** argv)
     return 0;
 }
 //main from pend math to be added
+/*
 int main(int argc, char** argv)
 {
 
@@ -28,3 +40,4 @@ int main(int argc, char** argv)
       step( &t, &theta, &omega);
    } 
 }
+*/
