337d99189072c3e2239fc857756788a77050312f
Dylan Resha <resha_dt@gsc_218_22.mercer.edu>
2019-01-17 11:30:55 -0500

    breaking both files into parts

--- main.cc
+++ main.cc
@@ -11,84 +11,8 @@ void init(void)
     glLoadIdentity();
 }
 
-void reshape (int w, int h)
-{
-   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
-   glMatrixMode (GL_PROJECTION);
-   glLoadIdentity();
-   gluPerspective(40.0, (GLfloat) w/(GLfloat) h, 1.0, 40.0);
-   glMatrixMode(GL_MODELVIEW);
-}
 
-void display(void)
-{
-
-    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-    glLoadIdentity();   // Call this before setting the viewing position 
-
-    // We are going to set our position to be down the Y-Axis looking at the
-    // center of the coordinate frame.  The positive Z-Axis will be up.
-
-    gluLookAt( 20.0,   0.0, 5.0,  // Eye
-                0.0,   0.0, 0.0,  // Center
-                0.0,   0.0, 1.0); // Up
-
-
-    glEnable(GL_DEPTH_TEST);
-
-    glColor3f(0.0,1.0,0.0);
-
-    // This should be to draw a cylinder 
-    glPushMatrix();
-    glTranslated(2.0,0.0,0.0);    
-    glColor3f (0.0,0.0,1.0);
-    gluCylinder(gluNewQuadric(), 
-            (GLdouble) 0.1,
-            (GLdouble) 0.1,
-            (GLdouble) 4.0,
-            (GLint)    20,
-            (GLint)    20 );
-    glPopMatrix();
-
-    // This should draw a green doughnut 
-    glPushMatrix();
-    glRotated(90.0,0,1,0);
-    glTranslatef(2.0,0.0,0.0);
-    glColor3f (0.0,1.0,0.0);
-    gluDisk(gluNewQuadric(), 
-            (GLdouble) 0.5,
-            (GLdouble) 1.0,
-            (GLint)     10,
-            (GLint)     20 );
-    glPopMatrix();
-
-    // This should draw a red sphere 
-    glPushMatrix();
-    glTranslatef(-3.0,1.0,1.0);
-    glColor3f (1.0,0.0,0.0);
-    gluSphere(gluNewQuadric(), 
-             (GLdouble) 1.5,
-             (GLint)     10,
-             (GLint)     10 );
-    glPopMatrix();
-
-    // This should draw a purple ellipsoid 
-    glPushMatrix();
-    glTranslatef(-3.0,1.0,-4.0);
-    glScalef(0.5,0.5,1.0);
-    glColor3f (1.0,0.0,1.0);
-    gluSphere(gluNewQuadric(), 
-             (GLdouble) 1.5,
-             (GLint)     10,
-             (GLint)     10 );
-    glPopMatrix();
-    
-
-    glFlush();
-
-}
-
-
+//main for objects
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
@@ -102,5 +26,16 @@ int main(int argc, char** argv)
     glutMainLoop();
     return 0;
 }
+//main from pend math to be added
+int main(int argc, char** argv)
+{
 
+   double t=0.0;
+   double theta=M_PI/4.0;
+   double omega=0.0;
 
+   for (long i=0; i<2*Nstep; i++ ) {
+      printf("%f %f %f\n", t, theta, omega);
+      step( &t, &theta, &omega);
+   } 
+}
--- pend.c
+++ pend.c
@@ -58,15 +58,4 @@ void step(double *t, double *theta, double *omega )
         *t = *t + h;
 }
 
-int main(int argc, char** argv)
-{
 
-   double t=0.0;
-   double theta=M_PI/4.0;
-   double omega=0.0;
-
-   for (long i=0; i<2*Nstep; i++ ) {
-      printf("%f %f %f\n", t, theta, omega);
-      step( &t, &theta, &omega);
-   } 
-}
