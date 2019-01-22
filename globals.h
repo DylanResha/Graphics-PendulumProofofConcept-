8e480701f057e79edecb653d49e7dae1431322db
Dylan Resha <resha_dt@gsc_218_22.mercer.edu>
2019-01-22 11:59:09 -0500

    need to fix multiple def

--- globals.h
+++ globals.h
@@ -6,6 +6,6 @@ unsigned frames=0;
 extern void display(void);
 extern void init(void);
 extern void fps(int);
-extern void fps(int, int);
-extern void step(double, double, double)
+extern void reshape(int, int);
+extern void step(double*, double*, double*);
 #endif
--- main.cc
+++ main.cc
@@ -1,8 +1,7 @@
 #include "includes.h"
 #include "globals.h"
 
-
- int main(int argc, char** argv)
+int main(int argc, char** argv)
 {
 
    double t=0.0;
